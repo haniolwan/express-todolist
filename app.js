7b99063d85813110b804bf6da1a617aa60239ffd
HaniOlwan <heenoow@gmail.com>
2022-09-25 11:30:38 +0300

    Create Signup Login Static Pages

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const { authRouter } = require('./router/auth');
 const app = express();
 
 app.set('port', 3333)
-app.use('/api',authRouter)
+app.use(authRouter)
 app.use(express.static('public'))
+app.use(express.static('views'))
+
 
 module.exports = {
     app
--- router/auth.js
+++ router/auth.js
@@ -1,9 +1,21 @@
+const { join } = require('path');
 const { Router } = require('express');
+
 
 const authRouter = Router();
 
 authRouter.get('/login', (req, res) => {
-    res.send("Hello From Login")
+    res.sendFile(join(__dirname, '..', 'views', 'login.html'));
+})
+authRouter.post('/login', (req, res) => {
+    res.send("Post login")
+})
+
+authRouter.get('/signup', (req, res) => {
+    res.sendFile(join(__dirname, '..', 'views', 'signup.html'));
+})
+authRouter.post('/signup', (req, res) => {
+    res.send("Post Signup")
 })
 
 module.exports = {
